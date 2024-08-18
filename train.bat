c846a5e4dedb3b91e34b8aebb1db3889d779f7dc
JiuYu77 <1992817771@qq.com>
2024-08-18 20:15:31 +0800

    modify

--- train.bat
+++ train.bat
@@ -1,8 +1,15 @@
 @echo off
+
+set python=E:\A_SoftwareInstall\python\python.exe
+set python_cpu=E:\A_SoftwareInstall\anaconda3\envs\flow\python.exe
+set python_gpu=E:\A_SoftwareInstall\anaconda3\envs\flow_cuda\python.exe
+
 if "%1" == "1" ( rem cpu
     E:\A_SoftwareInstall\anaconda3\envs\flow\python.exe .\script\press_cnn\train.py -d v4_wms
+    @REM %python_cpu% .\script\press_cnn\train.py -d v4_wms
 ) else if "%1" == "2" ( @REM cuda, gpu
     E:\A_SoftwareInstall\anaconda3\envs\flow_cuda\python.exe .\script\press_cnn\train.py -d v4_wms
+    @REM %python_gpu% .\script\press_cnn\train.py -d v4_wms
 ) else if %1 == 3 (
     E:\A_SoftwareInstall\anaconda3\envs\flow_cuda\python.exe .\script\press_cnn\train.py -d v4_diffPress
 ) else if %1 == 4 (
