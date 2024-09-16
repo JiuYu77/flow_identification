fe5fe4a0e71bb163e6020bbaabb29c4de1bd5a2c
JiuYu77 <1992817771@qq.com>
2024-09-16 22:03:16 +0800

    modify

--- model/nn/other/__init__.py
+++ model/nn/other/__init__.py
@@ -1,9 +1,2 @@
 # -*- coding: UTF-8 -*-
-import sys
-from pathlib import Path
-FILE = Path(__file__).resolve()
-root = FILE.parents[3]
-nn = FILE.parents[0]
-sys.path.append(str(nn))
-
-from model.nn.other.block2 import *
+from model.nn.other.block import *
