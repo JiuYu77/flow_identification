5033d467c0723c0224ec1f9a479294e3f2ed25b2
JiuYu77 <1992817771@qq.com>
2024-09-16 22:44:10 +0800

    modify

--- model/nn/other/block.py
+++ model/nn/other/block.py
@@ -4,7 +4,8 @@ from torch import nn
 class SENet1d(nn.Module):
     def __init__(self, channel, ratio=16):
         super(SENet1d, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        # self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.avg_pool = nn.AdaptiveMaxPool1d(1)
         self.fc = nn.Sequential(
                 nn.Linear(channel, channel // ratio, bias=False),
                 nn.ReLU(inplace=True),
