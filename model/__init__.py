7eee852c28e47e54494239d12b0fca2516218c01
JiuYu77 <1992817771@qq.com>
2024-11-06 17:12:50 +0800

    modify

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,7 @@
 import os
 import sys
 sys.path.append(os.getcwd())
-from model.yolo1d import *
+from model.yolo1d import YOLOv8_1D, YOLOv10_1D
 
 from model.nn.yolo import MODEL_YAML_DEFAULT
 
--- model/yolo1d/__init__.py
+++ model/yolo1d/__init__.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-# import sys
-# sys.path.append('.')
-# from model.yolo1d.yolov8_1D import MODEL_YAML_S, MODEL_YAML_DEFAULT, YOLOv8_1D
-# from model.yolo1d.yolov10_1D import YOLOv10_1D
-
-from .yolov8_1D import MODEL_YAML_S, MODEL_YAML_DEFAULT, YOLOv8_1D
-from .yolov10_1D import YOLOv10_1D
-
+import sys
+sys.path.append('.')
+from model.yolo1d.yolov8_1D import MODEL_YAML_S, MODEL_YAML_DEFAULT, YOLOv8_1D
+from model.yolo1d.yolov10_1D import YOLOv10_1D
 
 __all__ = ['MODEL_YAML_S', 'MODEL_YAML_DEFAULT', 'YOLOv8_1D', 'YOLOv10_1D']
