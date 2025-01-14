586749a6b49c831023d8207e596cc5e4d5335451
JiuYu77 <1992817771@qq.com>
2025-01-14 17:19:37 +0800

    modify

--- script/bash/init.sh
+++ script/bash/init.sh
@@ -14,7 +14,7 @@
 # pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 
 # CUDA 11.6
-# pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu116
 # pip install torch==1.13.0+cu116 torchvision==0.14.0+cu116 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
 
 # 3.安装其他Python包
