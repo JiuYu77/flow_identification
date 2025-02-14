b436b4862810d039f024b0ac40ba690ab5d269f0
JiuYu77 <1992817771@qq.com>
2025-02-14 09:13:44 +0800

    修改文件夹名字

---
+++ jyu/nn/yolo/conv.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+from torch import nn
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+class Conv1d(nn.Module):
+    """一维卷积，批规范化"""
+
+    default_act = nn.SiLU()  # 默认激活函数
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, d=1, g=1, act=True) -> None:
+        super().__init__()
+        self.conv1d = nn.Conv1d(c1, c2, k, s, autopad(k, p, d), dilation=d, groups=g, bias=False)
+        self.bn1d = nn.BatchNorm1d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn1d(self.conv1d(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv1d(x))
---
+++ jyu/nn/yolo/head.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+from torch import nn
+from conv import Conv1d
+import torch
+
+
+class Classify(nn.Module):
+    """YOLOv8 classification head, i.e. x(b,c1,20,20) to x(b,c2)."""
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, prob=0.2) -> None:
+        super().__init__()
+        c_ = 1280  # efficientnet_b0 size
+        self.conv = Conv1d(c1, c_, k, s, p, g)
+        self.pool = nn.AdaptiveAvgPool1d(1)
+        self.drop = nn.Dropout(p=prob, inplace=True)
+        self.linear = nn.Linear(c_, c2)
+
+    def forward(self, x):
+        if isinstance(x, list):
+            x = torch.cat(x, 1)
+        x = self.drop(self.pool(self.conv(x)).flatten(1))
+        x = self.linear(x)
+        return x if self.training else x.softmax(1)
