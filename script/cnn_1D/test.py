e5c61de6e5c49a25d877751c071de5b47f180f23
JiuYu77 <1992817771@qq.com>
2025-04-11 14:12:02 +0800

    modify

--- script/cnn_1D/test.py
+++ script/cnn_1D/test.py
@@ -15,6 +15,7 @@ def parse_args():
     netPath = os.path.join('result', 'train', '20250302.120903_YI-Netv2', 'weights', 'best_params.pt')
     netPath = os.path.join('result', 'train', '20250302.143001_YI-Netv2', 'weights', 'last_params.pt')
     netPath = os.path.join('result', 'train', '20250302.163810_YI-Netv2', 'weights', 'best_params.pt')
+    netPath = os.path.join('result', 'train', '20250326.133420_YI-Netv2', 'weights', 'best_params.pt')
 
 
     weight = netPath
