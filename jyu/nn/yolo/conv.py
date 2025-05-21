7209f855a637d094cd3aa616a52ad7e4f3e1719f
JiuYu77 <1992817771@qq.com>
2025-05-21 22:14:49 +0800

    :feat: update

--- jyu/nn/yolo/conv.py
+++ jyu/nn/yolo/conv.py
@@ -16,7 +16,7 @@ class Conv1d(nn.Module):
 
     def __init__(self, c1, c2, k=1, s=1, p=None, d=1, g=1, act=True) -> None:
         super().__init__()
-        self.conv1d = nn.Conv1d(c1, c2, k, s, autopad(k, p, d), dilation=d, groups=g, bias=False)  # 一维卷积kernel_size是k，不是 1xk
+        self.conv1d = nn.Conv1d(c1, c2, k, s, autopad(k, p, d), dilation=d, groups=g, bias=False)  # 一维卷积kernel_size是k，而不是 1xk
         self.bn1d = nn.BatchNorm1d(c2)
         self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
 
