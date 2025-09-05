73e51942176e682cd1c18397b76528190f38bed1
JiuYu77 <1992817771@qq.com>
2025-09-05 10:47:15 +0800

    rknn

--- script/rknn_script/to_rknn.py
+++ script/rknn_script/to_rknn.py
@@ -1,67 +1,60 @@
 # -*- coding: UTF-8 -*-
-
-from rknn.api import RKNN
 import sys
 sys.path.append(".")
 from jyu.utils import ph
+from jyu.rknn.rknn import ToRKNN
 
-
-# 初始化转换器
-rknn = RKNN(verbose=True)
 
 dynamic = True
 # dynamic = False
 
-# 配置模型参数
-if dynamic:
-    dynamic_input=[[[1, 1, 4096]], [[12, 1, 4096]], [[31, 1, 4096]]]
-else:
-    dynamic_input=None
-
-rknn.config(
-    # mean_values=[[123.675, 116.28, 103.53]],  # ImageNet标准化参数
-    # std_values=[[58.395, 58.395, 58.395]],
-    mean_values=[[123.675]],  # ImageNet标准化参数
-    std_values=[[58.395]],
-    target_platform="rk3588",
-    quantized_dtype="asymmetric_quantized-8",  # 启用INT8量化
-    dynamic_input=dynamic_input
-)
-
-# 加载ONNX模型
 time_dir = "20250603.175044_YI-Netv1"
 model_name = "YI-Netv1"
 
+# 配置模型参数
 if dynamic:
+    dynamic_input=[[[1, 1, 4096]], [[12, 1, 4096]], [[31, 1, 4096]]]
     model_name += "-dynamic_axes"
 else:
+    dynamic_input=None
     model_name += "-non_dynamic_axes"
 
 onnx_model = model_name + ".onnx"
 model = f"result/ONNX/{time_dir}/{onnx_model}"
-ret = rknn.load_onnx(model=model)
-if ret != 0:
-    print("Load ONNX failed!")
-    exit(ret)
 
-# 转换模型
-# ret = rknn.build(do_quantization=True, dataset="dataset.txt")  # 量化需提供校准数据集
-# ret = rknn.build()
-ret = rknn.build(do_quantization=False)
-if ret != 0:
-    print("Build RKNN failed!")
-    exit(ret)
-
-# 导出RKNN模型
 root = "./result/rknn/" + time_dir
 export_path = f"{root}/{model_name}.rknn"
 ph.checkAndInitPath(root)
 
-ret = rknn.export_rknn(export_path)
-if ret != 0:
-    print("Export RKNN failed!")
-    exit(ret)
+###############
+# from rknn.api import RKNN
+# rknn = RKNN(verbose=True)
+# rknn.config(
+#     # mean_values=[[123.675, 116.28, 103.53]],  # ImageNet标准化参数
+#     # std_values=[[58.395, 58.395, 58.395]],
+#     mean_values=[[123.675]],  # ImageNet标准化参数
+#     std_values=[[58.395]],
+#     target_platform="rk3588",
+#     quantized_dtype="asymmetric_quantized-8",  # 启用INT8量化
+#     dynamic_input=dynamic_input
+# )
+###############
 
-# 释放资源
-rknn.release()
 
+# 转换模型
+trknn = ToRKNN(
+    {"verbose":True},
+    {
+        "mean_values":[[123.675]],  # ImageNet标准化参数
+        "std_values": [[58.395]],
+        "target_platform":"rk3588",
+        "quantized_dtype":"asymmetric_quantized-8",  # 启用INT8量化
+        "dynamic_input":dynamic_input
+    },
+    {"model":model},
+    {"do_quantization":False},
+    {"export_path":export_path}
+)
+
+trknn.run()
+trknn.release()
