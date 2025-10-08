6fe1874dc419b5ff89fd3911dcd404e45ef0c2c7
JiuYu77 <1992817771@qq.com>
2025-10-08 19:36:57 +0800

    添加注释

--- jyu/rknn/rknn.py
+++ jyu/rknn/rknn.py
@@ -5,7 +5,7 @@ from rknn.api import RKNN
 class ToRKNN:
     def __init__(self, rknnArgs:dict, configArgs:dict, loadArgs:dict, buildArgs:dict, exportArgs:dict):
         self.rknn = RKNN(**rknnArgs)  # 初始化转换器
-        self.rknn.config(**configArgs)
+        self.rknn.config(**configArgs)  # 配置模型参数
         self.loadArgs = loadArgs
         self.buildArgs = buildArgs
         self.exportArgs = exportArgs
@@ -21,7 +21,7 @@ class ToRKNN:
 
     def _export_rknn(self): # 导出RKNN模型
         r = self.rknn.export_rknn(**self.exportArgs)
-        assert r == 0, "Export RKNN failed!"
+        assert r == 0, "Export RKNN failed!"    
 
     def release(self): # 释放资源
         self.rknn.release()
