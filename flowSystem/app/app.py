1ae6251eb2cf04970a7da207a32e475c62c35c9f
JiuYu77 <soramail@qq.com>
2025-11-24 16:07:01 +0800

    update

--- flowSystem/app/app.py
+++ flowSystem/app/app.py
@@ -4,13 +4,14 @@ from flask_cors import CORS
 import numpy as np
 
 import sys
-sys.path.append('.')
+
+sys.path.append(".")
 from jyu.rknn.rknnlite import RknnLite
 from jyu.dataset.flowDataset import FlowDataset
 from jyu.dataloader.flowDataLoader import FlowDataLoader
 
 app = Flask(__name__)
-CORS(app) # 跨域问题 (CORS), 前端页面和后端服务不在同一个域名下，可能会遇到 CORS 问题
+CORS(app)  # 跨域问题 (CORS), 前端页面和后端服务不在同一个域名下，可能会遇到 CORS 问题
 
 # 流型名称列表
 FLOW_TYPES = [
@@ -20,7 +21,7 @@ FLOW_TYPES = [
     "分层光滑流",
     "泡沫段塞流",
     "分层泡沫波浪流",
-    "泡沫环状流"
+    "泡沫环状流",
 ]
 rknn_model = "result/rknn/20250603.175044_YI-Netv1/YI-Netv1-dynamic_axes.rknn"
 rknn_lite = RknnLite(rknn_model)
@@ -28,29 +29,39 @@ rknn_lite = RknnLite(rknn_model)
 datasetPath = "/home/orangepi/flow/dataset/flow/v4/Pressure/4/val"
 transformName = "zScore_std"
 batchSize = 31
-shuffle=True
+shuffle = True
 sampleLength = 0
 step = 0
 
 dataset = None
 dataloader = None
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def index():
     return "Hello, World!"
 
-@app.route('/api/predict', methods=['POST'])
+
+@app.route("/api/predict", methods=["POST"])
 def predict():
     # 声明全局变量
     global sampleLength, step, dataset, dataloader
 
     req = request.json
-    length = int(req['sample_length'])
-    stp = int(req['step'])
+    length = int(req["sample_length"])
+    stp = int(req["step"])
     if sampleLength != length or step != stp:
         sampleLength = length
         step = stp
-        dataset = FlowDataset(datasetPath, sampleLength, step, transformName, clss=None, supervised=True, toTensor=False)
+        dataset = FlowDataset(
+            datasetPath,
+            sampleLength,
+            step,
+            transformName,
+            clss=None,
+            supervised=True,
+            toTensor=False,
+        )
         dataloader = FlowDataLoader(dataset, batchSize, shuffle)
 
     print("sampleLength=", sampleLength, "step=", step)
@@ -64,7 +75,7 @@ def predict():
             break
         ii += 1
         X = X.astype(np.float32)
-        outputs = rknn_lite(X) # 推理
+        outputs = rknn_lite(X)  # 推理
         # 后处理（示例：分类模型）
         pred_label = np.argmax(outputs, axis=2)
         probabilities = np.max(outputs, axis=2)
@@ -76,12 +87,14 @@ def predict():
     preLabel = int(pred_label[0][0])  # 预测标签
     probability = str(probabilities[0][0])  # 置信度，预测正确的概率
     x = X[0][0].tolist()
-    results.append({
-        "preLabel": preLabel,
-        "flowType": FLOW_TYPES[preLabel],
-        "flowData": x,
-        "probability": probability
-    })
+    results.append(
+        {
+            "preLabel": preLabel,
+            "flowType": FLOW_TYPES[preLabel],
+            "flowData": x,
+            "confidence": probability,
+        }
+    )
     print("Predicted class out:", pred_label)
     print("Predicted class out:", pred_label[0])
     print("Predicted class out:", int(pred_label[0][0]))
@@ -90,5 +103,6 @@ def predict():
 
     return jsonify({"results": results})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
